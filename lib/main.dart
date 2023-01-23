2fab1b2a418d2a8284d734f7571a68bbb2844423
Nicholaus Adhyatma <nicholausadhyatma@gmail.com>
2023-01-23 21:55:49 +0700

    map.list

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,117 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main() {
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  int initNumber = 1;
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  final List<Map<String, dynamic>> mylist = [
+    {
+      "name": "Nicholaus Adhyatma",
+      "age": 19,
+      "fav_color": [
+        "red",
+        "blue",
+        "yellow",
+        "red",
+        "blue",
+        "yellow",
+      ],
+    },
+    {
+      "name": "Dwikarna",
+      "age": 29,
+      "fav_color": [
+        "green",
+        "purple",
+        "red",
+      ],
+    },
+    {
+      "name": "Eden",
+      "age": 27,
+      "fav_color": [
+        "green",
+        "purple",
+        "red",
+      ],
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Indec Apps"),
+          title: const Text("Mapping List"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "$initNumber",
-              style: TextStyle(fontSize: initNumber.toDouble()),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextButton.icon(
-                  onPressed: () {
-                    setState(() {
-                      initNumber++;
-                    });
-                  },
-                  icon: const Icon(Icons.add),
-                  label: Text("Size now $initNumber"),
+        body: ListView(
+          children: mylist.map(
+            (data) {
+              List favColor = data["fav_color"];
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: Container(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          const CircleAvatar(),
+                          const SizedBox(
+                            width: 20,
+                            height: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Name : ${data["name"]}"),
+                              Text("Age : ${data["age"]}"),
+                              const SizedBox(
+                                height: 10,
+                                width: 10,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: favColor
+                              .map(
+                                (color) => Container(
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 5,
+                                  ),
+                                  margin: const EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                    vertical: 0,
+                                  ),
+                                  child: Text(
+                                    "$color",
+                                    style: const TextStyle(
+                                      color: Colors.blue,
+                                    ),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                TextButton.icon(
-                  onPressed: () {
-                    setState(() {
-                      initNumber > 1 ? initNumber-- : null;
-                    });
-                  },
-                  icon: const Icon(Icons.remove),
-                  label: const Text("Kurang"),
-                ),
-              ],
-            ),
-          ],
+              );
+            },
+          ).toList(),
         ),
       ),
     );
